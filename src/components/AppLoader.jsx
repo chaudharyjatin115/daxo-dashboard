55e20a2f0e0a8575502f0427d07a6f90d494c7d7
Jatin Chaudhary <chaudharyjatin116@proton.me>
2025-12-31 11:28:13 +0530

    Create AppLoader.jsx
    add few fixes

---
+++ src/components/AppLoader.jsx
@@ -0,0 +1,11 @@
+export default function AppLoader() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-64 space-y-4">
+        <div className="h-6 rounded bg-black/10 dark:bg-white/10 animate-pulse" />
+        <div className="h-6 rounded bg-black/10 dark:bg-white/10 animate-pulse" />
+        <div className="h-6 rounded bg-black/10 dark:bg-white/10 animate-pulse" />
+      </div>
+    </div>
+  );
+}
