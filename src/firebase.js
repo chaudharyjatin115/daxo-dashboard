0e4b7032cf434cf8fdb908954129ad279a38f751
Jatin Chaudhary <chaudharyjatin116@proton.me>
2026-01-01 22:46:37 +0530

    add something

--- src/firebase.js
+++ src/firebase.js
@@ -1,27 +1,17 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-
-
 // Import the functions you need from the SDKs you need
-
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDQg_oSG6HGTyG49r3SqOowQbik9-mu-eQ",
-  authDomain: "daxo-dashboard.firebaseapp.com",
-  projectId: "daxo-dashboard",
-  storageBucket: "daxo-dashboard.firebasestorage.app",
-  messagingSenderId: "1091453967262",
-  appId: "1:1091453967262:web:f440856e853b7753c9948f"
+  apiKey: "AIzaSyAQTAMquA2YMLpdTmpy9Pp2RJ0xsS4z0oA",
+  authDomain: "daxo-dashboard-9945c.firebaseapp.com",
+  projectId: "daxo-dashboard-9945c",
+  storageBucket: "daxo-dashboard-9945c.firebasestorage.app",
+  messagingSenderId: "95184496902",
+  appId: "1:95184496902:web:3b280ee5ae489c03629687"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
