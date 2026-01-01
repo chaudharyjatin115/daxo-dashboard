b57a1eb844c8b3bcb4b7c74e9b9b6565d9756795
Jatin Chaudhary <chaudharyjatin116@proton.me>
2026-01-01 22:56:21 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,22 @@
-const app = initializeApp(firebaseConfig);import { initializeApp } from "firebase/app";
+
+
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// Your web app's Firebase configuration
+
+/* firebase configuration */
 const firebaseConfig = {
   apiKey: "AIzaSyAQTAMquA2YMLpdTmpy9Pp2RJ0xsS4z0oA",
   authDomain: "daxo-dashboard-9945c.firebaseapp.com",
   projectId: "daxo-dashboard-9945c",
-  storageBucket: "daxo-dashboard-9945c.firebasestorage.app",
   messagingSenderId: "95184496902",
-  appId: "1:95184496902:web:3b280ee5ae489c03629687"
+  appId: "1:95184496902:web:3b280ee5ae489c03629687",
 };
 
+/* initialize app */
+const app = initializeApp(firebaseConfig);
 
+/* services */
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
