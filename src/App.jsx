fab286064a231585196f5d135ab02a8f9052e722
Jatin Chaudhary <chaudharyjatin116@proton.me>
2026-01-03 12:45:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Onboarding from "./pages/Onboarding";
 import Settings from "./pages/Settings";
-import AuthGate from "./components/AuthGate";
+import AuthGate from "./routes/AuthGate";
 
 export default function App() {
   return (
     <Routes>
-      {/* public */}
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
 
-      {/* protected */}
+      <Route path="/onboarding" element={<AuthGate><Onboarding /></AuthGate>} />
+
       <Route
-        path="/dashboard"
+        path="/"
         element={
           <AuthGate>
             <Dashboard />
-          </AuthGate>
-        }
-      />
-
-      <Route
-        path="/onboarding"
-        element={
-          <AuthGate>
-            <Onboarding />
           </AuthGate>
         }
       />
@@ -38,16 +28,6 @@ export default function App() {
         element={
           <AuthGate>
             <Settings />
-          </AuthGate>
-        }
-      />
-
-      {/* default */}
-      <Route
-        path="*"
-        element={
-          <AuthGate>
-            <Navigate to="/dashboard" replace />
           </AuthGate>
         }
       />
